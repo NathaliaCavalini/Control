17ce6f1f77ac0dd9a905bc5c5ab899cc7e18777f
Nathalia <tatacavalini@gmail.com>
2026-01-14 22:52:44 -0300

    adicionando funções

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import tkinter
 
+def novo_arquivo():
+    escrever_texto.delete(1.0, "end")
+
+def salvar_arquivo():
+    container = escrever_texto.get(1.0, "end")
+    with open("teste.txt", "w") as arquivo:
+        arquivo.write(container)
+
+def abrir_arquivo():
+    pass
+
+
+
 tela = tkinter.Tk()
 tela.title("Control") #Nome do software
 tela.geometry("640x480") #Tamanho da tela
 
-escrever_texto = tkinter.Text(tela, fonte="Arial 12")
+escrever_texto = tkinter.Text(tela, font="Arial 12")
+escrever_texto.pack()
 
 iniciar_menu = tkinter.Menu(tela) #Menu principal
 
 novo_menu = tkinter.Menu(iniciar_menu, tearoff=0) #Menu secundário
 
-novo_menu.add_command(label="Novo")
-novo_menu.add_command(label="Salvar")
-novo_menu.add_command(label="Abrir")
-novo_menu.add_command(label="Sair")
+novo_menu.add_command(label="Novo", command=novo_arquivo)
+novo_menu.add_command(label="Salvar", command=salvar_arquivo)
+novo_menu.add_command(label="Abrir", command=abrir_arquivo)
+novo_menu.add_command(label="Sair", command=tela.quit)
 
 iniciar_menu.add_cascade(label="Arquivo", menu=novo_menu) #Como vai aparecer o menu
 tela.config(menu=iniciar_menu)
